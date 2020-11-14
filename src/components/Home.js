3a5455b335d5fad2591112ca1a38e4788348232b
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2020-11-13 18:13:43 -0700

    api  renders photo

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,8 @@ import {Link} from 'react-router-dom';
 
 export default function Home(){
     return(
-        <div>
-            <Link to='/nasaphoto'>Your Nasa Photo of Thhe Day!</Link>
+        <div className='home'>
+            <Link className='home-link' to='/nasaphoto'>Your Nasa Photo of Thhe Day!</Link>
         </div>
-    )
+    );
 }
--- src/components/NasaPhoto.js
+++ src/components/NasaPhoto.js
@@ -1,40 +1,46 @@
-import React from 'react';
-import {useState, useEffect} from 'react'; 
+import  {useState, useEffect } from 'react'
+import React from 'react'
 
-export default function NasaPhoto(){
-
+export default function NasaPhoto() {
     const [photoData, setPhotoData] = useState(null);
 
     useEffect(() => {
-        fetchPhoto();
-        
-        async function fetchPhoto () {
-        const res = await fetch(
-            `https://api.nasa.gov/planetary/apod?api_key=klgrtIHeC0JWi8CZoZXemCw9XTMhbSceX7eFE6D0`
-
-        );
-        const data = await res.json();
-        
-        setPhotoData(data);
-        console.log (data)
+        fetchPhoto()
 
 
+        async function fetchPhoto() {
+            const res = await fetch(
+                `https://api.nasa.gov/planetary/apod?api_key=F2mJpauIJG1YfVbW4NMkOE1VmBTf5UUkT0VctRjl`
+            );
 
-    }
+            const data = await res.json();
+            setPhotoData(data);
+            console.log(data)
+        }
+    
 }, []);
 
-if (!photoData) return <div/>;
 
+if (!photoData) return <div/>
 
 return (
     <div>
+
         <img 
-        src={photoData.url} 
-        alt={photoData.title}
-        />
+
+
+        src={photoData.url}
+        alt={photoData.title}/>
+
+        
+<div>
+<h1> {photoData.title}</h1>
+<p>{photoData.date}</p>
+<p>{photoData.explanation}</p>
+
+</div>
+    
     </div>
 
 )
-
-
 }
