36ccc001eeb0a6bf773ef7aab74702f0b5af0a3f
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2023-11-03 13:28:52 -0600

    possible change

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom';
 export default function Home(){
     return(
         <div className='home'>
-            <Link className='home-link' to='/nasaphoto' >NASA APOD</Link>
+            <Link className='home-link' to='/nasaphoto' >The Diggin Pit</Link>
         </div>
     );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,13 +3,23 @@ import { Link } from 'react-router-dom';
 
 export default function NavBar() {
     return (
-        <div className="navbar">
-            <ul>
-                <Link className="link" to="/" exact >Home</Link>
-                
-            </ul>
-        </div>
-    )
+      <div className="navbar">
+        <ul>
+          <Link className="link" to="/" exact>
+            Home
+          </Link>
+          <Link className="link" to="/" exact>
+            About
+          </Link>
+          <Link className="link" to="/" exact>
+            FAQ
+          </Link>
+          <Link className="link" to="/" exact>
+            Contact
+          </Link>
+        </ul>
+      </div>
+    );
 }
 
 
